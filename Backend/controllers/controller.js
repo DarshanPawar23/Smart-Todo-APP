d5e75673065e5e17b8fd7c45c27429adf12cc546
dheeraj123495 <dheerajsp0700@gmail.com>
2026-05-09 22:51:14 +0530

    Login

--- Backend/controllers/controller.js
+++ Backend/controllers/controller.js
@@ -32,3 +32,47 @@ export const register = async(req,res)=>{
         return res.status(500).json(error);
     }
 }
+
+export const login = (req, res) => {
+
+    const {email, password} = req.body;
+
+    const query = "SELECT * FROM Users WHERE email = ?";
+
+    db.query(query, [email], async (err, result) => {
+        
+        if(err){
+         return res.status(400).json({
+            message : "user not found"
+          })
+        }
+        if(result.length === 0){
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const user = result[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                message: "Invalid Password"
+            });
+        }
+
+        const token = jwt.sign(
+            {id: user.id},
+            process.env.JWT_SECRET,
+            {expiresIn: "1d"}
+        );
+
+        res.status(200).json({
+            message: "Login Success",
+            token
+        });
+
+    });
+
+};
--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -1,9 +1,10 @@
-import {register} from "../controllers/controller.js"
+import {register,login } from "../controllers/controller.js";
+// import middleware from "../middleware/middleware.js";
 import express from  "express";
 
 const route = express.Router();
 
 route.post("/register",register);
-
+route.post("/login",login)
 export default route;
 
